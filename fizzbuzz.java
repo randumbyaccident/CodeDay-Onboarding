df6e4b7ab245ea50b8ccde353c7e6bed19ca4b4b
Aaron Liang <98863604+randumbyaccident@users.noreply.github.com>
2026-07-01 13:25:47 -0700

    Add files via upload

---
+++ fizzbuzz.java
@@ -0,0 +1,14 @@
+public class fizzbuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            if (i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            }
+        }
+    }
+}
